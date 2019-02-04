0fb99e97628150f48bc3ee05850088da78ffe8a3
N3Cr0N <n3cr0n@list.ru>
2019-02-04 23:55:48 +0100

    Update Modules.php

--- third_party/MX/Modules.php
+++ third_party/MX/Modules.php
@@ -44,7 +44,7 @@ spl_autoload_register('Modules::autoload');
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * */
+ **/
 class Modules
 {
     public static $routes;
